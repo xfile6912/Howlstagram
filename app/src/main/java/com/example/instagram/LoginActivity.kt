00b3fb2b70579a4ed0ea01ba29166576d28e602a
xfile6912 <xfile6912@naver.com>
2021-01-08 17:05:14 +0900

    Add facebook login and google login

--- app/src/main/java/com/example/instagram/LoginActivity.kt
+++ app/src/main/java/com/example/instagram/LoginActivity.kt
@@ -10,23 +10,32 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
+import java.util.*
 
 
 class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? =null
     var googleSignInClient:GoogleSignInClient?= null
     var GOOGLE_LOGIN_CODE=9001
+    var callbackManager:CallbackManager?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -37,16 +46,24 @@ class LoginActivity : AppCompatActivity() {
         google_sign_in_button.setOnClickListener {
             googleLogin()
         }
+        facebook_login_button.setOnClickListener {
+            facebookLogin()
+        }
         var gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
         googleSignInClient=GoogleSignIn.getClient(this, gso)
-        printHashKey()
+        // printHashKey()
+        callbackManager= CallbackManager.Factory.create()
     }
+    //IoFl2ecigifgJPf6pacN+Z0ZotI=
     //dnl/EO6/1AiO3YatvlgZ95qy5yo=
+
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
         if(requestCode==GOOGLE_LOGIN_CODE){
             var result= Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             if(result!!.isSuccess){
@@ -59,6 +76,37 @@ class LoginActivity : AppCompatActivity() {
     {
         var credential=GoogleAuthProvider.getCredential(account?.idToken,null)
         auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //Login
+                    moveMainPage(task.result?.user)
+                } else {
+                    //Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    fun facebookLogin(){
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object:FacebookCallback<LoginResult>{
+                override fun onSuccess(result: LoginResult?) {
+                    handlerFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onCancel() {
+                }
+
+                override fun onError(error: FacebookException?) {
+                }
+
+            })
+    }
+    fun handlerFacebookAccessToken(token:AccessToken?) {
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+           auth?.signInWithCredential(credential)
             ?.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     //Login
