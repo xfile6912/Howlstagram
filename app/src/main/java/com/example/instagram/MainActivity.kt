1e21aa3f97188871ba2cd8475c2dd56302d4ed92
xfile6912 <xfile6912@naver.com>
2021-01-08 18:28:58 +0900

    Add fragments and add photo activity Link with fire base storage

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.instagram
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.instagram.navigation.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId)
+        {
+            R.id.action_home->{
+                var detailViewFragment=DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                return true;
+            }
+            R.id.action_search->{
+                var gridFragment=GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                return true;
+            }
+            R.id.action_add_photo->{
+                if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED)
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                    return true;
+            }
+            R.id.action_favorite_alarm->{
+                var alarmFragment=AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                return true;
+            }
+            R.id.action_account->{
+                var userFragment=UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                return true;
+            }
+        }
+        return false
     }
 }
